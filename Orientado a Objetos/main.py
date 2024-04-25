735a9a554a65fd40bd33e599d1d1751ca00e9d8b
Iago-Vargas <166236052+Iago-Vargas@users.noreply.github.com>
2024-04-25 14:09:23 -0300

    Add files via upload

---
+++ Orientado a Objetos/main.py
@@ -0,0 +1,13 @@
+from pessoa import Pessoa
+
+p1 = Pessoa('Luiz', 29)
+p2 = Pessoa('João', 23)
+
+p1.comer('maça')
+p1.falar ('Oi')
+p2.falar ('Oi')
+p1.parar_comer()
+p1.falar ('Oi')
+p1.comer('maça')
+p1.parar_falar()
+p1.comer('maça')
